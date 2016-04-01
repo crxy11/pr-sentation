4726b26817b3fd017baa4c822bc7d79537a72b70
Jan Lagarden <jan.lagarden@live.de>
2016-04-01 22:38:54 +0200

    Initial Commit

---
+++ self/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  self
+//
+//  Created by Jan Lagarden on 01.04.16.
+//  Copyright © 2016 Jan Lagarden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
