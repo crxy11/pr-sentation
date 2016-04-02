1bee0b0d75a96336ded0a20012e4e2eb167c6b41
Jan Lagarden <jan.lagarden@live.de>
2016-04-02 13:15:52 +0200

    working version

--- self/ViewController.swift
+++ self/ViewController.swift
@@ -8,11 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var test: UIImageView!
+    @IBOutlet weak var selfieView: UIImageView!
+    @IBOutlet var down: UISwipeGestureRecognizer!
+    @IBOutlet var up: UISwipeGestureRecognizer!
+    
+    var selectImage = true
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        selfieView.contentMode = .ScaleAspectFill
+        selfieView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        selectImage = true
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func swipeUp(sender: AnyObject) {
+        
+        UIGraphicsBeginImageContextWithOptions(selfieView.bounds.size, false, UIScreen.mainScreen().scale)
+        selfieView.drawViewHierarchyInRect(selfieView.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        showPicker()
+    }
+    
+    @IBAction func swipeDown(sender: AnyObject) {
+        self.showPicker()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        test.hidden = true
+        self.selfieView.userInteractionEnabled = true
+        self.selfieView.addGestureRecognizer(down)
+        self.selfieView.addGestureRecognizer(up)
+        self.selfieView.addSubview(test)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func showPicker() {
+        selectImage = false
+        let picker = UIImagePickerController()
+        let screenBounds: CGSize = UIScreen.mainScreen().bounds.size
+        let scale = screenBounds.height / screenBounds.width
+        
+        picker.delegate = self
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        picker.allowsEditing = false
+        picker.cameraViewTransform = CGAffineTransformScale(picker.cameraViewTransform, scale, scale)
+        
+        self.presentViewController(picker, animated: false, completion: nil)
+        
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if(selectImage) {
+            test.hidden = true
+            showPicker()
+            selectImage = false
+        } else {
+            test.hidden = false
+        }
     }
 
     override func didReceiveMemoryWarning() {
